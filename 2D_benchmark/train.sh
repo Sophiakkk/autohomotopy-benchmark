5748471f95050845c8a0ef0341d347f47a3a5735
Sophiakkk <zhuzhuhsm@gmail.com>
2023-08-13 19:05:37 -0400

    no space in sh

--- 2D_benchmark/train.sh
+++ 2D_benchmark/train.sh
@@ -8,9 +8,7 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:2
 
-func_list = ('ackley', 'holder_table', 'cross_in_tray', 'bukin', 'drop_wave', 
-                 'eggholder', 'griewank', 'langermann', 'levy', 'levy_13',
-                 'rastrigin', 'schaffer2', 'schaffer4', 'schwefel', 'shubert')
+func_list = ('ackley', 'holder_table', 'cross_in_tray', 'bukin', 'drop_wave','eggholder', 'griewank', 'langermann', 'levy', 'levy_13','rastrigin', 'schaffer2', 'schaffer4', 'schwefel', 'shubert')
 method_list = ('autohomotopy', 'pinns')
 
 for func in ${func_list[@]}
