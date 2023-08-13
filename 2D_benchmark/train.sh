6bdb5f708a48c343e19100bd263966f263cd903f
Sophiakkk <zhuzhuhsm@gmail.com>
2023-08-13 18:55:01 -0400

    modify sh

--- 2D_benchmark/train.sh
+++ 2D_benchmark/train.sh
@@ -8,8 +8,8 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --gres=gpu:2
 
-for func in ["ackley", "holder_table", "cross_in_tray", "bukin", "drop_wave", 
-                "eggholder", "griewank", "langermann", "levy", "levy_13",
-                "rastrigin", "schaffer2", "schaffer4", "schwefel", "shubert"]
-    for method in ["autohomotopy", "pinns"]
+for func in [ackley, holder_table, cross_in_tray ,   bukin  ,   drop_wave  , 
+                  eggholder  ,   griewank  ,   langermann  ,   levy  ,   levy_13  ,
+                  rastrigin  ,   schaffer2  ,   schaffer4  ,   schwefel  ,   shubert  ]
+    for method in [  autohomotopy  ,   pinns  ]
         python train.py -f ${func} -m ${method}
