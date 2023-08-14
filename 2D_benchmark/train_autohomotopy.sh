8a0f6cc0c1caaeb9db57fe238c31f3365915e5fa
Sophiakkk <zhuzhuhsm@gmail.com>
2023-08-14 14:47:09 -0400

    separate training for different methods

---
+++ 2D_benchmark/train_autohomotopy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --job-name=benchmark_train_autohomotopy
+#SBATCH --output=benchmark_train_autohomotopy.log
+#SBATCH --error=benchmark_train_autohomotopy.err
+#SBATCH --mem=16G
+#SBATCH --ntasks=4
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:2
+
+func_list=("ackley"   "holder_table"  "cross_in_tray" "bukin" "drop_wave" "eggholder" "griewank"  "langermann"    "levy"  "levy_13"   "rastrigin" "schaffer2" "schaffer4" "schwefel"  "shubert")
+method_list=("autohomotopy")
+
+for func in ${func_list[@]}; do
+    echo $func
+    for method in ${method_list[@]}; do
+        echo $method
+        python train.py --func $func --method $method
+        sleep 5
+    done
+done
