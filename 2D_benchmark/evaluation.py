c627bbe3b957abbc7c788450bfef6a78f968697d
Sophiakkk <zhuzhuhsm@gmail.com>
2023-08-15 17:48:42 -0400

    fix func name issues

--- 2D_benchmark/evaluation.py
+++ 2D_benchmark/evaluation.py
@@ -8,28 +8,39 @@ import timeit
 # Configuation
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--seed", type=int, default=0)
-parser.add_argument("-m","--method_name", type = str, default = "AutoHomotopy")
+parser.add_argument("-m","--method_name", type = str, default = "GD")
 parser.add_argument("-f","--func_name", type = str, default = "ackley")
+parser.add_argument("-n","--num_iter", type = int, default = 10000)
+parser.add_argument("-beta", "--step_size", type = float, default = 0.001)
 args = parser.parse_args()
 
 # Parameters
 seed = args.seed
+seed_list = [1,2,3,4,5,6,7,8,9,10]
+# "ackley","bukin",
+func_list = ["dropwave","eggholder","griewank","langermann","levy","levy13","rastrigin","schaffer2","schwefel","shubert"]
 method_name = args.method_name
 func_name = args.func_name
 x_range = np.array(domain_range[func_name])
+x_opt = np.array(opt_solutions[func_name][0]) # single opt solution
+total_iterations = args.num_iter
+step_size = args.step_size
 
-if method_name == 'AutoHomotopy':
-    algorithm = AutoHomotopyTrainer(net=NeuralNet(), 
-                                    x_range = x_range, 
-                                    init_func_name=func_name, 
-                                    method = method_name)
-    algorithm.preprocess()
-    algorithm.train()
+if method_name == 'GD':
+    for func_name in func_list:
+        for seed in seed_list:
+            algorithm_evaluator = GDEvaluator(init_func_name = func_name, 
+                                            seed=seed,
+                                            x_range=x_range, 
+                                            x_opt = x_opt)
+            algorithm_evaluator.initalizer()
+            algorithm_evaluator.evaluate()
 
-elif method_name == 'pinns':
-    algorithm = PINNsTrainer(net=NeuralNet(),
-                            x_range = x_range,
-                            init_func_name=func_name,
-                            method = method_name)
-    algorithm.preprocess()
-    algorithm.train()
+elif method_name == 'SLGD_r':
+    for seed in seed_list:
+        algorithm_evaluator = SLGH_r_Evaluator(init_func_name = func_name, 
+                                        seed=seed,
+                                        x_range=x_range, 
+                                        x_opt = x_opt)
+        algorithm_evaluator.initalizer()
+        algorithm_evaluator.evaluate()
